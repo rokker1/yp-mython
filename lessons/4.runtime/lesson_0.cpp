d120e3d04c40ceb56af09a1f8d76bc1b7edd6d5b
alexander@MaiBook-M Ubuntu 22.04 <darkriderification@yandex.ru>
2022-08-25 20:11:03 +0500

    runtime precode

--- lessons/4.runtime/lesson_0.cpp
+++ lessons/4.runtime/lesson_0.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <sstream>
 #include <unordered_map>
+#include <vector>
+#include <memory>
 
 class Context {
 public:
@@ -15,6 +17,10 @@ struct DummyContext : Context {
     }
 
     std::ostringstream output; // Юнит-тесты могут проверять содержимое потока output
+};
+
+struct Object {
+
 };
 
 class ObjectHolder {
@@ -41,3 +47,44 @@ private:
 };
 
 using Closure = std::unordered_map<std::string, ObjectHolder>;
+
+class Executable {
+public:
+    virtual ~Executable() = default;
+    virtual ObjectHolder Execute(Closure& closure, Context& context) = 0;
+};
+
+struct Method {
+    // имя метода
+    std::string name;
+    // имена формальных параметров метода
+    std::vector<std::string> formal_params;
+    // тело метода
+    std::unique_ptr<Executable> bode;
+
+};
+
+class Class : public Object {
+public:
+    explicit Class(std::string name, std::vector<Method> methods, const Class* parent);
+    [[nodiscard]] const Method* GetMethod(const std::string& name) const;
+};
+
+class ClassInstance : public Object {
+public:
+    explicit ClassInstance(const Class& cls);
+    ObjectHolder Call(const std::string& method, 
+        const std::vector<ObjectHolder>& actual_args,
+        Context& context
+    );
+    [[nodiscard]] bool HasMethod(const std::string& method, 
+                size_t argument_count) const;
+
+    [[nodiscard]] Closure& Fields();
+    [[nodiscard]] const Closure& Fields() const;
+
+};
+
+int main() {
+    return 0;
+}
