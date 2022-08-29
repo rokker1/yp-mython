262ba4fd35e69d4654fe3394b4eacd8f9c8f6df8
Alexander Zhavoronkov -EasyNote-TE11HC <darkriderification@yandex.ru>
2022-08-29 08:57:59 +0500

    interpreter precode

---
+++ lessons/5.interpreter/lexer.cpp
@@ -0,0 +1,367 @@
+#include "lexer.h"
+
+namespace parse {
+
+
+
+static const unordered_map<std::string, Token> mython_keywords = {
+    {"class"s, Token{token_type::Class{}}},
+    {"return"s, Token{token_type::Return{}}}, 
+    {"if"s, Token{token_type::If{}}}, 
+    {"else"s, Token{token_type::Else{}}}, 
+    {"def"s, Token{token_type::Def{}}}, 
+    {"print"s, Token{token_type::Print{}}}, 
+    {"or"s, Token{token_type::Or{}}}, 
+    {"None"s, Token{token_type::None{}}}, 
+    {"and"s, Token{token_type::And{}}}, 
+    {"not"s, Token{token_type::Not{}}}, 
+    {"True"s, Token{token_type::True{}}}, 
+    {"False"s, Token{token_type::False{}}}
+};
+
+bool operator==(const Token& lhs, const Token& rhs) {
+    using namespace token_type;
+
+    if(lhs.index() != rhs.index()) {
+        return false;
+    }
+    if (lhs.Is<Char>()) {
+        return lhs.As<Char>().value == rhs.As<Char>().value;
+    }
+    if (lhs.Is<Number>()) {
+        return lhs.As<Number>().value == rhs.As<Number>().value;
+    }
+    if (lhs.Is<String>()) {
+        return lhs.As<String>().value == rhs.As<String>().value;
+    }
+    if (lhs.Is<Id>()) {
+        return lhs.As<Id>().value == rhs.As<Id>().value;
+    }
+    return true;
+}
+
+bool operator!=(const Token& lhs, const Token& rhs) {
+    return !(lhs == rhs);
+}
+
+std::ostream& operator<<(std::ostream& os, const Token& rhs) {
+    using namespace token_type;
+
+#define VALUED_OUTPUT(type) \
+    if (auto p = rhs.TryAs<type>()) return os << #type << '{' << p->value << '}';
+
+    VALUED_OUTPUT(Number);
+    VALUED_OUTPUT(Id);
+    VALUED_OUTPUT(String);
+    VALUED_OUTPUT(Char);
+
+#undef VALUED_OUTPUT
+
+#define UNVALUED_OUTPUT(type) \
+    if(rhs.Is<type>()) return os << #type;
+
+    UNVALUED_OUTPUT(Class);
+    UNVALUED_OUTPUT(Return);
+    UNVALUED_OUTPUT(If);
+    UNVALUED_OUTPUT(Else);
+    UNVALUED_OUTPUT(Def);
+    UNVALUED_OUTPUT(Newline);
+    UNVALUED_OUTPUT(Print);
+    UNVALUED_OUTPUT(Indent);
+    UNVALUED_OUTPUT(Dedent);
+    UNVALUED_OUTPUT(And);
+    UNVALUED_OUTPUT(Or);
+    UNVALUED_OUTPUT(Not);
+    UNVALUED_OUTPUT(Eq);
+    UNVALUED_OUTPUT(NotEq);
+    UNVALUED_OUTPUT(LessOrEq);
+    UNVALUED_OUTPUT(GreaterOrEq);
+    UNVALUED_OUTPUT(None);
+    UNVALUED_OUTPUT(True);
+    UNVALUED_OUTPUT(False);
+    UNVALUED_OUTPUT(Eof);
+
+#undef UNVALUED_OUTPUT
+
+    return os << "Unknown token :("sv;
+}
+
+Lexer::Lexer(std::istream& input)
+    : input_(input)
+    , current_indent_(0)
+    , indent_buf_(0)
+    
+
+{
+    current_token_ = LoadToken(input);
+}
+
+const Token& Lexer::CurrentToken() const {
+    return *current_token_;
+}
+
+Token Lexer::NextToken() {
+    current_token_ = LoadToken(input_);
+    return *current_token_;
+}
+
+Token LoadString(std::istream& input, char exit_quote) {
+    auto it = std::istreambuf_iterator<char>(input);
+    auto end = std::istreambuf_iterator<char>();
+    std::string s;
+    
+    while(true) {
+        if(it  == end) {
+            throw LexerError("String parsing error"s);
+        }
+        const char ch = *it;
+        if (ch == exit_quote) {
+            ++it;
+            break;
+        } else if (ch == '\\') {
+            ++it;
+            if(it  == end) {
+                throw LexerError("String parsing error"s);
+            }
+            const char escaped_char = *(it);
+            switch (escaped_char) {
+                case 'n':
+                    s.push_back('\n');
+                    break;
+                case 't':
+                    s.push_back('\t');
+                    break;
+                case '\'':
+                    s.push_back('\'');
+                    break;
+                case '"':
+                    s.push_back('"');
+                    break;
+                
+                default:
+                    throw LexerError("Unrecognized escape sequence \\"s + escaped_char);
+            }
+
+        } else if (ch == '\n' || ch == '\r') {
+            throw LexerError("Unexpected end of line"s);
+        } else {
+            s.push_back(ch);
+        }
+        ++it;
+    }
+
+    return Token(token_type::String{std::move(s)});
+}
+Token LoadNumber(std::istream& input) {
+    std::string parsed_sum;
+
+    // Считывает в parsed_num очередной символ из input
+    auto read_char = [&parsed_sum, &input] {
+        parsed_sum += static_cast<char>(input.get());
+        if(!input) {
+            throw LexerError("Failed to read number from stream"s);
+        }
+    };
+
+    auto read_digits = [&input, read_char] {
+        if(!std::isdigit(input.peek())) {
+            throw LexerError("A digit is expected"s);
+        }
+        while (std::isdigit(input.peek())) {
+            read_char();
+        }
+    };
+
+    read_digits();
+
+    try
+    {
+        return Token(token_type::Number{std::stoi(parsed_sum)});
+    }
+    catch(...)
+    {
+        throw LexerError("Failed to convert "s + parsed_sum + " to number"s);
+    }
+}
+
+Token LoadId(std::istream& input) {
+    std::string s;
+    char c;
+    while (c = input.peek(), std::isalnum(c) || c == '_') {
+        s.push_back(static_cast<char>(input.get()));
+    }
+    if(mython_keywords.count(s)) {
+        return mython_keywords.at(s);
+    }
+    return Token(token_type::Id{s});
+}
+
+std::optional<size_t> Lexer::GetIndent() {
+    size_t updated_indetnt = 0; // отступ, который считаем для текущей строки
+    while(input_.peek() == ' ') {
+        input_.get();
+        ++updated_indetnt;
+    }
+    if(input_.peek() == '#') {
+        std::string s;
+        no_newline_chars_ = false;
+        getline(input_, s);
+        return std::nullopt;
+    }
+    if(input_.peek() == '\n') {
+        // прочитана пустая строка из пробелов
+        input_.get();
+        return std::nullopt;
+    }
+
+    return updated_indetnt;
+}
+
+Token Lexer::LoadToken(std::istream& input) {
+    // если токен пустой (читаем первый токен)
+    // или последний - текущий токен - новая линия - 
+    // прочитать отступ
+    // сравнить с хранимым отступом
+    // обновить хранимый отступ
+    // вернуть токен с отступом
+    if(current_token_ == Token(token_type::Newline{}) || !current_token_.has_value()) {
+        std::optional<size_t> indent = GetIndent();
+        
+        if(indent.has_value()) {
+            if(*indent % 2 != 0) {
+                throw LexerError("Not even indent used!"s);
+            }
+
+            indent_buf_ = *indent;
+        } else {
+            return LoadToken(input);
+        }
+    }
+
+    if(indent_buf_ < current_indent_) {
+        current_indent_ -= 2;
+        return Token(token_type::Dedent{});
+    } else if (indent_buf_ > current_indent_) {
+        current_indent_ += 2;
+        return Token(token_type::Indent{});
+    }
+
+
+    char c;
+
+    if(input.peek() == '\n') {
+        input.get();
+        no_newline_chars_ = false;
+        return Token(token_type::Newline{});
+    }
+    if(!(input >> c)) {
+        if(no_newline_chars_) {
+            no_newline_chars_ = false;
+            return Token(token_type::Newline{});
+        }
+        return Token{token_type::Eof()};
+    }
+    if(c == '#') {
+        std::string s;
+        no_newline_chars_ = false;
+        getline(input, s);
+        return Token(token_type::Newline{});
+    }
+    if(c == '\n') {
+        no_newline_chars_ = false;
+        return Token(token_type::Newline{});
+    }
+
+    if(std::isdigit(c)) {
+        input.putback(c);
+        return LoadNumber(input);
+    }
+
+    if(std::isalpha(c) || c == '_') {
+        input.putback(c);
+        return LoadId(input);
+    }
+
+    if(c == '-') {
+        return Token(token_type::Char{'-'});
+    }
+
+    if(c == '\'') {
+        return LoadString(input, '\'');
+    } 
+
+    if(c == '"') {
+        return LoadString(input, '"');
+    }
+        
+    if(c == '+') {
+        return Token(token_type::Char{'+'});
+    }
+
+    if(c == '-') {
+        return Token(token_type::Char{'-'});
+    }
+
+    if(c == '*') {
+        return Token(token_type::Char{'*'});
+    }
+
+    if(c == '/') {
+        return Token(token_type::Char{'/'});
+    }
+
+    if(c == ':') {
+        return Token(token_type::Char{':'});
+    }
+    if(c == '(') {
+        return Token(token_type::Char{'('});
+    }
+    if(c == ')') {
+        return Token(token_type::Char{')'});
+    }
+    if(c == ',') {
+        return Token(token_type::Char{','});
+    }
+    if(c == '.') {
+        return Token(token_type::Char{'.'});
+    }
+
+    if(c == '>') {
+        if(input.peek() == '=') {
+            input.get();
+            return Token(token_type::GreaterOrEq{});
+        } else {
+            return Token(token_type::Char{'>'});
+        }
+    }
+
+    if(c == '<') {
+        if(input.peek() == '=') {
+            input.get();
+            return Token(token_type::LessOrEq{});
+        } else {
+            return Token(token_type::Char{'<'});
+        }
+    }
+
+    if(c == '=') {
+        if(input.peek() == '=') {
+            input.get();
+            return Token(token_type::Eq{});
+        } else {
+            return Token(token_type::Char{'='});
+        }
+    }
+    if(c == '!') {
+        if(input.peek() == '=') {
+            input.get();
+            return Token(token_type::NotEq{});
+        } else {
+            return Token(token_type::Char{'!'});
+        }
+    }
+    return Token(token_type::Id{});
+}
+
+
+}
---
+++ lessons/5.interpreter/runtime.cpp
@@ -0,0 +1,248 @@
+#include "runtime.h"
+using namespace std;
+namespace runtime {
+
+ObjectHolder::ObjectHolder(std::shared_ptr<Object> data)
+    : data_(std::move(data)) {
+}
+
+void ObjectHolder::AssertIsValid() const {
+    assert(data_ != nullptr);
+}
+
+ObjectHolder ObjectHolder::Share(Object& object) {
+    // Возвращаем невладеющий shared_ptr (его deleter ничего не делает)
+    return ObjectHolder(std::shared_ptr<Object>(&object, [](auto* /*p*/) { /* do nothing */ }));
+}
+
+ObjectHolder ObjectHolder::None() {
+    return ObjectHolder();
+}
+
+Object& ObjectHolder::operator*() const {
+    AssertIsValid();
+    return *Get();
+}
+
+Object* ObjectHolder::operator->() const {
+    AssertIsValid();
+    return Get();
+}
+
+Object* ObjectHolder::Get() const {
+    return data_.get();
+}
+
+ObjectHolder::operator bool() const {
+    return Get() != nullptr;
+}
+
+bool IsTrue(const ObjectHolder& object) {
+    // проверяем что object не пуст
+    if(object) {
+        if(Bool* p = object.TryAs<Bool>()) {
+            return p->GetValue();
+        } else if (Number* p = object.TryAs<Number>()) {
+            return p->GetValue() != 0;
+        } else if (String* p = object.TryAs<String>()) {
+            return !(p->GetValue().empty());
+        }
+    }
+    return false;
+}
+
+void ClassInstance::Print(std::ostream& os, Context& context) {
+    // Заглушка, реализуйте метод самостоятельно
+    if(HasMethod("__str__", 0)) {
+        ObjectHolder string_representation = Call("__str__", {}, context);
+        if(string_representation) { // если холдер не пуст
+            String* s_object_ptr = string_representation.TryAs<String>();
+            s_object_ptr->Print(os, context);
+        }
+    } else {
+        size_t obj_address = *(reinterpret_cast<size_t*>(this));
+        os << obj_address;
+    }
+}
+
+bool ClassInstance::HasMethod(const std::string& method, size_t argument_count) const {
+    
+    const Method* m = cls_.GetMethod(method);
+    // поиск у базового класса
+    if(!m) {
+        if(cls_.GetParent()) {
+            m = cls_.GetParent()->GetMethod(method);
+        }
+
+    }
+    
+    if(m) {
+        if(m->formal_params.size() == argument_count) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+   throw runtime_error("e"s);
+}
+
+Closure& ClassInstance::Fields() {
+    return fields_;
+}
+
+const Closure& ClassInstance::Fields() const {
+    return fields_;
+}
+
+ClassInstance::ClassInstance(const Class& cls) 
+    : cls_(cls)
+{
+    
+}
+
+ObjectHolder ClassInstance::Call(const std::string& method,
+                                 const std::vector<ObjectHolder>& actual_args,
+                                 Context& context) {
+    
+    fields_.clear();
+    fields_.insert({"self", ObjectHolder::Share(*this)});
+
+    size_t args_count = 0;
+    if(!actual_args.empty()) {
+        args_count = actual_args.size();
+    }
+
+    if(HasMethod(method, args_count)) {
+        const Method* m = cls_.GetMethod(method);
+        if(!m) {
+            if(cls_.GetParent()) {
+                m = cls_.GetParent()->GetMethod(method);
+            }
+
+        }
+        size_t params_count = m->formal_params.size();
+        for(size_t i = 0; i < params_count; ++i) {
+            fields_.insert({m->formal_params.at(i), actual_args.at(i)});
+        }
+        return m->body.get()->Execute(fields_, context);
+        
+    } else {
+        throw runtime_error("No such method!"s);
+    }
+}
+
+Class::Class(std::string name, std::vector<Method> methods, const Class* parent) 
+    : name_(name)
+    , methods_(std::move(methods))
+    , parent_(parent)
+{
+
+}
+
+const Method* Class::GetMethod(const std::string& name) const {
+    std::vector<Method>::const_iterator method = std::find_if(
+                methods_.begin(), methods_.end(), 
+                [&name](const runtime::Method& method){
+                    return method.name == name;
+                });
+    if(method != methods_.end()) {
+        return &(*method);
+    }
+    return nullptr;
+}
+
+[[nodiscard]] const std::string& Class::GetName() const {
+    return name_;
+}
+
+void Class::Print(ostream& os, [[maybe_unused]] Context& context) {
+    os << "Class "s << name_ ;
+}
+
+void Bool::Print(std::ostream& os, [[maybe_unused]] Context& context) {
+    os << (GetValue() ? "True"sv : "False"sv);
+}
+
+bool Equal(const ObjectHolder& lhs, const ObjectHolder& rhs, Context& context) {
+    if(Number* n_left = lhs.TryAs<Number>()) {
+        if (Number* n_right = rhs.TryAs<Number>()) {
+            return n_left->GetValue() == n_right->GetValue();
+        }
+    }
+    if(String* s_left = lhs.TryAs<String>()) {
+        if (String* s_right = rhs.TryAs<String>()) {
+            return s_left->GetValue() == s_right->GetValue();
+        }
+    }
+    if(Bool* b_left = lhs.TryAs<Bool>()) {
+        if (Bool* b_right = rhs.TryAs<Bool>()) {
+            return b_left->GetValue() == b_right->GetValue();
+        }
+    }
+
+    if(ClassInstance* left = lhs.TryAs<ClassInstance>()) {
+        if(left->HasMethod("__eq__", 1)) {
+            ObjectHolder result = left->Call("__eq__", {rhs}, context);
+            if(result) {
+                Bool* r = result.TryAs<Bool>();
+                return r->GetValue();
+            } 
+        }
+    }
+
+    if(!lhs && !rhs) {
+        return true;
+    }
+
+    throw std::runtime_error("Cannot compare objects for equality"s);
+}
+
+bool Less(const ObjectHolder& lhs, const ObjectHolder& rhs, Context& context) {
+    if(Number* n_left = lhs.TryAs<Number>()) {
+        if (Number* n_right = rhs.TryAs<Number>()) {
+            return n_left->GetValue() < n_right->GetValue();
+        }
+    }
+    if(String* s_left = lhs.TryAs<String>()) {
+        if (String* s_right = rhs.TryAs<String>()) {
+            return s_left->GetValue() < s_right->GetValue();
+        }
+    }
+    if(Bool* b_left = lhs.TryAs<Bool>()) {
+        if (Bool* b_right = rhs.TryAs<Bool>()) {
+            return b_left->GetValue() < b_right->GetValue();
+        }
+    }
+    if(ClassInstance* left = lhs.TryAs<ClassInstance>()) {
+        if(left->HasMethod("__lt__", 1)) {
+            ObjectHolder result = left->Call("__lt__", {rhs}, context);
+            if(result) {
+                Bool* r = result.TryAs<Bool>();
+                return r->GetValue();
+            } 
+        }
+    }
+
+    throw std::runtime_error("Cannot compare objects for less"s);
+}
+
+bool NotEqual(const ObjectHolder& lhs, const ObjectHolder& rhs, Context& context) {
+    return !(Equal(lhs, rhs, context));
+}
+
+bool Greater(const ObjectHolder& lhs, const ObjectHolder& rhs, Context& context) {
+    return (!(Less(lhs, rhs, context)) && NotEqual(lhs, rhs, context));
+    throw std::runtime_error("Cannot compare objects for equality"s);
+}
+
+bool LessOrEqual(const ObjectHolder& lhs, const ObjectHolder& rhs, Context& context) {
+    return !Greater(lhs, rhs, context);
+    throw std::runtime_error("Cannot compare objects for equality"s);
+}
+
+bool GreaterOrEqual(const ObjectHolder& lhs, const ObjectHolder& rhs, Context& context) {
+    return !Less(lhs, rhs, context);
+    throw std::runtime_error("Cannot compare objects for equality"s);
+}
+}
